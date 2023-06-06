e4f811144e607752343b2175c8908d9b9376229f
Arsalan Fahim <arsalan_fahim@hotmail.com>
2023-06-05 19:21:52 -0600

    Added all input data for GGE bill

---
+++ GGEUtilityBill.py
@@ -0,0 +1,44 @@
+print ("Welcome to Global Energy bill calculator")
+
+Account_Number = input("Enter your account number: ")
+print(Account_Number)
+
+Month = input("Enter the month number (e.g, for January, enter 1): ")
+print(Month)
+
+Electricity_Plan = input("Enter your electricity plan (EFIR or EFLR): ")
+print(Electricity_Plan)
+
+Electricity_Usage = input("Enter the amount of electricity you used in month " + Month + " (in KWh): ")
+print(Electricity_Usage)
+
+
+Gas_Plan = input("Enter your gas plan (GFIR or GFLR):")
+print(Gas_Plan)
+
+Gas_Usage = input("Enter the amount of gas you used in month " + Month + " (in GJ): ")
+print(Gas_Plan)
+
+Province = input("Enter the abbreviation for your province of residence (two letters): ")
+print(Province)
+
+fixed_monthly_fee = (120.62)
+
+
+print("Thank you! Your total amount due now is:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
